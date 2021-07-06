ae427b4d1d6e23d4213742f80b71bb9e0fa75d4a
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2021-07-06 18:47:16 -0400

    changed to rofi

--- .inputrc
+++ .inputrc
@@ -2,6 +2,7 @@
 export ZDOTDIR=~/.config/zsh
 export LESSHISTFILE=~/.config/less
 export XDG_CONFIG_HOME=~/.config
+export XDG_DATA_HOME=~/.local/share
 export EDITOR=/usr/bin/nvim
 export VISUAL=/usr/bin/nvim
 
--- install.sh
+++ install.sh
@@ -5,13 +5,25 @@
 SCRIPTPATH=$(dirname $(realpath $0))
 
 # set symbolic links
-ln -s "$SCRIPTPATH/vimb"		$XDG_CONFIG_HOME
-ln -s "$SCRIPTPATH/nvim"		$XDG_CONFIG_HOME
-ln -s "$SCRIPTPATH/zathura"	$XDG_CONFIG_HOME
-ln -s "$SCRIPTPATH/sxhkd"		$XDG_CONFIG_HOME
-ln -s "$SCRIPTPATH/bspwm"		$XDG_CONFIG_HOME
-ln -s "$SCRIPTPATH/dunst"		$XDG_CONFIG_HOME
-ln -s "$SCRIPTPATH/compton"	$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/sxhkd"			$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/bspwm"			$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/dunst"			$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/compton"		$XDG_CONFIG_HOME
+
+ln -s "$SCRIPTPATH/polybar"		$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/rofi"			$XDG_CONFIG_HOME
+
+mkdir $XDG_CONFIG_HOME/zsh
+ln -s "$SCRIPTPATH/zsh/.zshrc"	$XDG_CONFIG_HOME/zsh
+ln -s "$SCRIPTPATH/nvim"			$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/zathura"		$XDG_CONFIG_HOME
+ln -s "$SCRIPTPATH/alacritty"		$XDG_CONFIG_HOME
+
+ln -s "$SCRIPTPATH/.xbindkeys"	~
+ln -s "$SCRIPTPATH/.xinitrc"		~
+ln -s "$SCRIPTPATH/.zshenv"		~
+ln -s "$SCRIPTPATH/.inputrc"		~
+ln -s "$SCRIPTPATH/scripts"		~
 
 # install zsh syntax highligithing
 git clone "https://github.com/zsh-users/zsh-syntax-highlighting" $SCRIPTPATH/zsh/zsh-syntax-highlighting
